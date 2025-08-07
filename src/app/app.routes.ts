5381cf89a5399504a8116da06fa2d090d7093f63
giovanasilverio <giovanasilveriopereiratp@gmail.com>
2025-08-07 19:24:10 -0300

    scss button e input label

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { List } from './modules/to-do-list/pages/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: List
+    }
+
+];
